6c8a4a7a8b3da788e2e1e3a37f80e4bd5946abe9
dien <dien404@163.com>
2023-01-11 21:42:24 +0800

    集成user的购买车票功能

--- src/component/user/TicketBuy.jsx
+++ src/component/user/TicketBuy.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
-import { Form, Input, Button } from "antd";
-import TicketTable from "./TicketTable";
+import React, { useState, useContext } from "react";
+import { Form, Input, Button, Popconfirm, Table } from "antd";
+import { RouterContext } from "../../router";
 
 export default function TicketBuy() {
+    // 全局消息提醒
+    const { messageShow } = useContext(RouterContext);
     // 展示数据
     const [dataSource, setDataSource] = useState([
         {
@@ -14,12 +16,71 @@ export default function TicketBuy() {
     ]);
     // 数据库数据最大数
     const [dataSourceTotal, setDataSourceTotal] = useState(undefined);
-    // 处理表单提交
+    // 当前表格页码
+    const [currentPage, setCurrentPage] = useState(1);
+    // 表格展示最大数
+    const [tablePageSize, setTablePageSize] = useState(10);
+    // 处理表单提交(搜索功能)
     const handleFormSubmit = (values) => {
         setDataSource([]);
         setDataSourceTotal();
         console.log(values);
     };
+    // 处理页码变化(表格页码)
+    const handlePageChange = (page, pageSize) => {
+        setCurrentPage(page);
+        setTablePageSize(pageSize);
+    };
+    // 处理购买Button(购买车票)
+    const handleBuyTicket = (trainNumber) => {
+        messageShow("购买 " + trainNumber + " 车次车票成功", "info");
+    };
+    // 渲染表格项
+    const columns = [
+        {
+            title: "起点",
+            dataIndex: "origin",
+            key: "origin",
+            render: (record) => (
+                <div className="flex flex-col items-center">
+                    <span>{record}</span>
+                    <span className="text-xs">发车时间</span>
+                </div>
+            ),
+        },
+        {
+            title: "目的地",
+            dataIndex: "destination",
+            key: "destination",
+            render: (record) => (
+                <div className="flex flex-col items-center">
+                    <span>{record}</span>
+                    <span className="text-xs">抵达时间</span>
+                </div>
+            ),
+        },
+        { title: "车次号", dataIndex: "trainNumber", key: "trainNumber" },
+        { title: "剩余车票", dataIndex: "ticketNumber", key: "ticketNumber" },
+        {
+            title: "操作",
+            dataIndex: "trainNumber",
+            key: "index",
+            render: (record) => (
+                <>
+                    <Popconfirm
+                        title={`确定购买 ${record} 车次车票嘛？`}
+                        okText="确定"
+                        cancelText="取消"
+                        onConfirm={() => {
+                            handleBuyTicket(record);
+                        }}
+                    >
+                        <Button type="default">购买{record}</Button>
+                    </Popconfirm>
+                </>
+            ),
+        },
+    ];
     return (
         <div className="flex flex-col">
             <Form
@@ -58,7 +119,17 @@ export default function TicketBuy() {
                 </Form.Item>
             </Form>
             <div className="">
-                <TicketTable dataSource={dataSource} total={dataSourceTotal} />
+                <Table
+                    dataSource={dataSource}
+                    columns={columns}
+                    pagination={{
+                        current: currentPage,
+                        tablePageSize: tablePageSize,
+                        onChange: handlePageChange,
+                        total: dataSourceTotal,
+                        hideOnSinglePage: true,
+                    }}
+                ></Table>
             </div>
         </div>
     );

f4dc027f21ceb4cbbd0ccfb40cd0bc2497dae84f
dien <dien404@163.com>
2023-01-11 22:29:49 +0800

    管理员页面初步设计

---
+++ src/component/admin/AddTrainDialog.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AddTrainDialog() {
+    return <div>AddTrainDialog</div>;
+}
--- src/page/Admin.jsx
+++ src/page/Admin.jsx
@@ -1,5 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { Table, Form, Input, Button } from "antd";
 
 export default function Admin() {
-    return <div className="">Admin</div>;
+    const [dataSource, setDataSource] = useState([]);
+    // 处理查询车次
+    const handleFormSubmit = (values) => {
+        console.log(values);
+    };
+    // 处理车次添加
+    const handleTrainAdd = () => {
+        console.log("添加车次");
+    };
+    return (
+        <div className="p-5 flex flex-col">
+            {/* 表单 */}
+            <Form
+                onFinish={handleFormSubmit}
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                }}
+            >
+                <Form.Item
+                    label="起点"
+                    name="origin"
+                    style={{ marginRight: "10px" }}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="目的地"
+                    name="destination"
+                    style={{ marginRight: "10px" }}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="车次号"
+                    name="trainNumber"
+                    style={{ marginRight: "10px" }}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="default" htmlType="submit">
+                        查询
+                    </Button>
+                    <Button
+                        type="default"
+                        style={{ marginLeft: "10px" }}
+                        onClick={handleTrainAdd}
+                    >
+                        添加车次
+                    </Button>
+                </Form.Item>
+            </Form>
+            <div className="">
+                <Table dataSource={dataSource} />
+            </div>
+        </div>
+    );
 }
